2b898424f59f80ef8c0135af67285cbdfd708410
Mike Flinn <mflinn92@gmail.com>
2021-06-16 21:43:32 -0400

    ppm tests

--- src/ppm.rs
+++ src/ppm.rs
@@ -21,10 +21,12 @@ impl<W: Write> Ppm<W> {
 
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
     use super::*;
 
     #[test]
-    fn test_write_header() {
+    fn write_header() {
         let writer = Vec::new();
         let mut ppm = Ppm::new(writer);
 
@@ -32,5 +34,41 @@ mod tests {
         let height = 40;
 
         ppm.header(width, height).unwrap();
+        let expected = format!("P3\n{} {}\n255\n", width, height);
+        assert_eq!(expected, String::from_utf8(ppm.writer).unwrap());
+    }
+
+    #[test]
+    fn ppm_append() {
+        let writer = Vec::new();
+        let mut ppm = Ppm::new(writer);
+
+        let point = "1 23 120";
+        ppm.append(point).unwrap();
+        let expected = format!("{}\n", point);
+        assert_eq!(expected, String::from_utf8(ppm.writer).unwrap());
+    }
+
+    #[test]
+    fn ppm_write() {
+        let writer = Vec::new();
+        let mut ppm = Ppm::new(writer);
+
+        let width = 80;
+        let height = 60;
+
+        ppm.header(width, height).unwrap();
+
+        let points = vec!["1 2 3", "4 5 6", "7 8 9"];
+        for point in &points {
+            ppm.append(point).unwrap();
+        }
+
+        let expected = format!(
+            "P3\n{} {}\n255\n{}\n{}\n{}\n",
+            width, height, points[0], points[1], points[2],
+        );
+
+        assert_eq!(expected, String::from_utf8(ppm.writer).unwrap());
     }
 }
